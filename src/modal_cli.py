4f0518fb78ba75b3b916f3ffeb2383065d55d027
HonraVT <111161607+HonraVT@users.noreply.github.com>
2024-01-24 12:31:15 -0300

    Update modal_cli.py

--- src/modal_cli.py
+++ src/modal_cli.py
@@ -1,6 +1,13 @@
 import requests
 
-from src.secret import USER_AGENT, API_SECRET_TOKEN, MODAL_URL, DEFAULT_ERROR_RESPONSE, LOGGING_ENABLED
+from src.secret import (
+    USER_AGENT,
+    API_SECRET_TOKEN,
+    MODAL_URL,
+    MODAL_BOT_DESCRIPTION,
+    DEFAULT_ERROR_RESPONSE,
+    LOGGING_ENABLED
+)
 
 headers = {
     "User-Agent": USER_AGENT,
@@ -14,6 +21,7 @@ def receive_data(msg: str, history: list[list] | None = None) -> list[str]:
 
     payload = {
         "key": API_SECRET_TOKEN,
+        "bot_description": MODAL_BOT_DESCRIPTION,
         "prompts": [
             {
                 "prompt": msg,
@@ -30,25 +38,3 @@ def receive_data(msg: str, history: list[list] | None = None) -> list[str]:
         return [msg, response[0]["response"]]
     except requests.RequestException:
         return [msg, DEFAULT_ERROR_RESPONSE]
-
-
-# import modal
-# from src.secret import MODAL_APP_NAME, DEFAULT_ERROR_RESPONSE
-#
-#
-# def receive_data(msg: str, history: list[list] | None = None) -> list[str]:
-#     if len(msg) <= 1:
-#         return [msg, DEFAULT_ERROR_RESPONSE]
-#     try:
-#         f = modal.Function.lookup(MODAL_APP_NAME, "Model.generate")
-#         res = f.remote([
-#             {
-#                 "prompt": msg,
-#                 "history": [] if not history else history
-#             }
-#         ])
-#         if LOGGING_ENABLED:
-#             print(res)
-#         return [msg, res[0]["response"]]
-#     except:
-#         return [msg, DEFAULT_ERROR_RESPONSE]
