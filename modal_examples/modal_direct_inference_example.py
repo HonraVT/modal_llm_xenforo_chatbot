493e240c06cb4d5ad8f285f87803a47445d27024
HonraVT <111161607+HonraVT@users.noreply.github.com>
2024-01-24 12:28:32 -0300

    Update modal_direct_inference_example.py

--- modal_examples/modal_direct_inference_example.py
+++ modal_examples/modal_direct_inference_example.py
@@ -1,16 +1,17 @@
-# First, register on modal.com using your GitHub account.
-# Steps:
-# 1. Create a new virtual environment with Python 3.11.
-# 2. Install the Modal library (add 'modal' to requirements.txt) with: `pip install modal`
-# 3. Don't worry about IDE errors from imports inside functions; Modal will run the code in the cloud.
-# 4. Configure Modal (get the token authorization file): `python -m modal setup`
-# 5. Create a Hugging Face token at https://huggingface.co/settings/tokens to avoid download weight blocks.
-# 6. Now, set a new "secrets" on the Modal dashboard in the "Secrets" tab.
-# 7. Select "custom" and add a key: HUGGINGFACE_TOKEN, value: "your Hugging Face token," and a name: my-huggingface-secret.
-# 8. Afterward, run this file with: `modal deploy modal_direct_inference_example.py`
-# 9. Wait for Modal to internally install and set "MODAL_APP_NAME" in src.secret.py file with your app name (line 67).
-# 10. Comment out "modal_cli api client" in the secrets file.
-# 11. Change the code in src.modal_cli.py with the code in modal_cli.py from this folder.
+# first you need register on modal.com with your GitHub account.
+# steps:
+# Create a new virtual environment with python 3.11.
+# install modal lib (add modal to requirements.txt) with: pip install modal
+# Don't worry about errors in your IDE from imports inside functions, Modal will run the code in the cloud.
+# Configure modal (get token authorization file): python -m modal setup
+# create a huggingface token at https://huggingface.co/settings/tokens to not be blocked at download weights.
+# now set a new "secrets" on modal dashboard -> secrets tab
+# select "custom" add key: HUGGINGFACE_TOKEN value: "your huggingface token" and add a name: my-huggingface-secret
+# After run this file with: modal deploy modal_direct_inference_example.py
+# Wait modal internally installs and set "MODAL_APP_NAME" in src.secret.py file with your app name (line 67).
+# comment "modal_cli api client" from secrets file.
+# and change code in src.modal_cli.py with code in modal_cli.py from this folder.
+
 
 import os
 
@@ -37,12 +38,14 @@ def download_model_to_folder():
     move_cache()
 
 
-def format_prompts(prompts: list[dict]) -> list[str]:
+def format_prompts(bot_description: str, prompts: list[dict]) -> list[str]:
     result = []
     for item in prompts:
-        output = ("### System:\nYou are Vtlore a helpful AI assistant. You always answer briefly in a few words, "
-                  "at most two or three lines of text and never create long lists. You always respond in Brazilian "
-                  "Portuguese\n\n")
+        output = (
+            f"### System:\nYou are {bot_description}. You always answer briefly in a few words, "
+            "at most two or three lines of text and never create long lists. You always respond in Brazilian "
+            "Portuguese\n\n"
+        )
         for q, a in item["history"]:
             output += f"### User:\n{q}\n\n### Assistant:\n{a}\n\n"
         output += f"### User:\n{item['prompt']}\n\n### Assistant:"
@@ -74,10 +77,10 @@ class Model:
         self.llm = LLM(MODEL_DIR)
 
     @method()
-    def generate(self, user_questions: list[dict]) -> list[dict]:
+    def generate(self, bot_desc: str, user_questions: list[dict]) -> list[dict]:
         from vllm import SamplingParams
 
-        prompts = format_prompts(user_questions)
+        prompts = format_prompts(bot_desc, user_questions)
 
         sampling_params = SamplingParams(
             temperature=0.75,
