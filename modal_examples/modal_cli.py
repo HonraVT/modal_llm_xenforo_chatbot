0d2ae99f81b8c894235ded0dc5a05e93dc9c99b5
HonraVT <111161607+HonraVT@users.noreply.github.com>
2024-01-24 12:29:50 -0300

    Update modal_cli.py

--- modal_examples/modal_cli.py
+++ modal_examples/modal_cli.py
@@ -1,7 +1,7 @@
 # # modal direct inference, see: modal_direct_inference_example.py
 
 import modal
-from src.secret import MODAL_APP_NAME, DEFAULT_ERROR_RESPONSE
+from src.secret import MODAL_APP_NAME, MODAL_BOT_DESCRIPTION, DEFAULT_ERROR_RESPONSE
 
 
 def receive_data(msg: str, history: list[list] | None = None) -> list[str]:
@@ -9,12 +9,15 @@ def receive_data(msg: str, history: list[list] | None = None) -> list[str]:
         return [msg, DEFAULT_ERROR_RESPONSE]
     try:
         f = modal.Function.lookup(MODAL_APP_NAME, "Model.generate")
-        res = f.remote([
-            {
-                "prompt": msg,
-                "history": [] if not history else history
-            }
-        ])
+        res = f.remote(
+            MODAL_BOT_DESCRIPTION,
+            [
+                {
+                    "prompt": msg,
+                    "history": [] if not history else history
+                }
+            ]
+        )
         return [msg, res[0]["response"]]
     except:
         return [msg, DEFAULT_ERROR_RESPONSE]
