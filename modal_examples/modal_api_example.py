a3835812ba3b22b703f393ab9050d6bc1bcfed73
HonraVT <111161607+HonraVT@users.noreply.github.com>
2024-01-29 01:34:21 -0300

    Update modal_api_example.py
    Use quantitized model to fit on low cost T4 GPU
    Increase timeouts.

--- modal_examples/modal_api_example.py
+++ modal_examples/modal_api_example.py
@@ -1,17 +1,14 @@
-# first you need register on modal.com with your GitHub account.
-# steps:
-# Create a new virtual environment (separate from this project) with python 3.11.
-# install modal lib: pip install modal
-# Don't worry about errors in your IDE, the modal will run the code in the cloud.
-# Configure modal (get token authorization file): python -m modal setup
-# create a huggingface token at https://huggingface.co/settings/tokens to not be blocked at download weights.
-# now set a new "secrets" on modal dashboard -> secrets tab
-# select "custom" add key: HUGGINGFACE_TOKEN value: "your huggingface token" and add a name: my-huggingface-secret
-# create another secret on modal dashboard -> secrets tab:
-# select "custom" key: API_TOKEN value: "your random 16 length confidential password" and add a name: bot-secrets
-# After, run this file with: modal deploy modal_api_example.py
-# Wait modal internally installs and get your url endpoint printed on console :)
-# set this url in "MODAL_URL" in src.secret.py file.
+# Register on modal.com with your GitHub account. Follow these steps:
+# 1. Create a new virtual environment with Python 3.11.
+# 2. Install the Modal library: `pip install modal`.
+# 3. Ignore IDE errors; Modal will execute the code in the cloud.
+# 4. Configure Modal and obtain the token authorization file: `python -m modal setup`.
+# 5. Generate a Hugging Face token at https://huggingface.co/settings/tokens to avoid download issues.
+# 6. On the Modal dashboard's "Secrets" tab, create a new secret named "my-huggingface-secret" with key: `HUGGINGFACE_TOKEN` and value: "your Hugging Face token."
+# 7. Create another secret named "bot-secrets" with key: `API_TOKEN` and value: "your random 16-character confidential password."
+# 8. Run the file using: `modal deploy modal_api_example.py`.
+# 9. Wait for Modal to install and get your URL endpoint printed on the console.
+# 10. Set this URL as "MODAL_URL" in the `src.secret.py` file.
 
 
 import os
@@ -22,7 +19,10 @@ from modal import Image, Secret, Stub, method, web_endpoint
 from pydantic import BaseModel
 
 MODEL_DIR = "/model"
-BASE_MODEL = "w4r10ck/SOLAR-10.7B-Instruct-v1.0-uncensored"
+
+# use quantitized model to fit on low cost T4 GPU
+BASE_MODEL = "TheBloke/SOLAR-10.7B-Instruct-v1.0-uncensored-AWQ"
+# BASE_MODEL = "w4r10ck/SOLAR-10.7B-Instruct-v1.0-uncensored"
 
 
 def download_model_to_folder():
@@ -37,21 +37,6 @@ def download_model_to_folder():
         token=os.environ["HUGGINGFACE_TOKEN"],
     )
     move_cache()
-
-
-def format_prompts(bot_description: str, prompts: list[dict]) -> list[str]:
-    result = []
-    for item in prompts:
-        output = (
-            f"### System:\nYou are {bot_description}. You always answer briefly in a few words, "
-            "at most two or three lines of text and never create long lists. You always respond in Brazilian "
-            "Portuguese\n\n"
-        )
-        for q, a in item["history"]:
-            output += f"### User:\n{q}\n\n### Assistant:\n{a}\n\n"
-        output += f"### User:\n{item['prompt']}\n\n### Assistant:"
-        result.append(output)
-    return result
 
 
 image = (
@@ -70,7 +55,29 @@ image = (
 stub = Stub("example-vllm-inference", image=image)
 
 
-@stub.cls(gpu="A100", secret=Secret.from_name("my-huggingface-secret"))
+def format_prompts(bot_description: str, prompts: list[dict]) -> list[str]:
+    result = []
+    for item in prompts:
+        output = (
+            f"### System:\nYou are {bot_description}. You always answer briefly in a few words, "
+            "at most two or three lines of text and never create long lists. You always respond in coherent and "
+            "syntactically correct Brazilian Portuguese\n\n"
+        )
+        h = item.get("history")
+        if h:
+            for q, a in h:
+                output += f"### User:\n{q}\n\n### Assistant:\n{a}\n\n"
+        output += f"### User:\n{item['prompt']}\n\n### Assistant:"
+        result.append(output)
+    return result
+
+
+@stub.cls(
+    gpu="T4",
+    timeout=60 * 10,
+    container_idle_timeout=60 * 10,
+    secret=Secret.from_name("my-huggingface-secret")
+)
 class Model:
     def __enter__(self):
         from vllm import LLM
@@ -95,11 +102,12 @@ class Model:
         for index, output in enumerate(result):
             num_tokens += len(output.outputs[0].token_ids)
             res.append(
-                {"status": 0,
-                 "prompt": user_questions[index]["prompt"],
-                 "response": output.outputs[0].text,
-                 "tokens": num_tokens
-                 }
+                {
+                    "status": 0,
+                    "prompt": user_questions[index]["prompt"],
+                    "response": output.outputs[0].text,
+                    "tokens": num_tokens
+                }
             )
         return res
 
@@ -110,7 +118,7 @@ class Item(BaseModel):
     prompts: list
 
 
-@stub.function(secret=modal.Secret.from_name("vt-bot-secrets"))
+@stub.function(timeout=60 * 10, secret=modal.Secret.from_name("vt-bot-secrets"))
 @web_endpoint(method="POST")
 def f(item: Item):
     if item.key != os.environ["API_TOKEN"]:
@@ -121,3 +129,4 @@ def f(item: Item):
     model = Model()
     res = model.generate.remote(item.bot_description, item.prompts)
     return res
+
